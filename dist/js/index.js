9a24f23ee3461b21bba617d27d16549c256a9c64
julian-hecker <julianheckerdev@gmail.com>
2020-01-08 23:14:19 -0500

    added chart

--- dist/js/index.js
+++
@@ -1,6 +0,0 @@
-function toggleNav() {
-    document.querySelector('.js-nav-bar-button')
-        .classList.toggle('nav-bar__button--active');
-    document.querySelector('.js-nav-bar-nav')
-        .classList.toggle('nav-bar__nav--active');
-}
